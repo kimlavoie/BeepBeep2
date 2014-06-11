29225ff859d75e4f646a287b21020193f511416b
Kim Lavoie <kim.lavoie.14@gmail.com>
2014-06-11 16:41:23 -0400

    Fonctionne avec Ruby, mais il faut ajouter une ligne dans le script. A corriger

--- ExternalProcessor.java
+++ ExternalProcessor.java
@@ -9,24 +9,22 @@ public class ExternalProcessor{
     Process p; 
     BufferedWriter writer;
     BufferedReader reader;
+    String directory = "./";
 
   public ExternalProcessor(String program) throws Exception{
-    ProcessBuilder pb = new ProcessBuilder(program);
+    ProcessBuilder pb = new ProcessBuilder(directory + program);
     Map<String, String> env = pb.environment();
     env.put("PYTHONUNBUFFERED", "true");
     p = pb.redirectErrorStream(true).start(); 
     writer = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
     reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-    run("event: {x: 0}");
-    run("event: {x: 34}");
   }
   
   public void run(String event) throws Exception{
     writer.write(event + "\n");
     writer.flush();
     String line;
-    while((line = reader.readLine()).equals("")){}
+    while((line = reader.readLine()).equals("")){System.out.println("empty line");}
     System.out.println(line);
   }
 
@@ -35,7 +33,19 @@ public class ExternalProcessor{
   }
 
   public static void main(String args[]) throws Exception{
-    ExternalProcessor p = new ExternalProcessor("./test.py");
+    System.out.println("PYTHON");
+    System.out.println("------");
+    ExternalProcessor p = new ExternalProcessor("test.py");
+    p.run("event: {x: 0}");
+    p.run("event: {x: 34}");
     p.cleanup();
+
+    System.out.println("");
+    System.out.println("RUBY");
+    System.out.println("----");
+    ExternalProcessor p2 = new ExternalProcessor("test.rb");
+    p2.run("event: {x: 0}");
+    p2.run("event: {x: 10}");
+    p2.cleanup();
   }
 }
---
+++ test.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+$stdout.sync = true
+while true
+  c = gets.chomp
+  puts c
+end
